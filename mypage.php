b2a39b4528f02e8f8e971e5b95a9c1cc05914927
hsazzad2-prog <hsazzad02@gmail.com>
2021-07-27 09:40:23 +0600

    First commit

---
+++ mypage.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> My Home Page </title>
+</head>
+<body>
+
+<h1>My Home page</h1>
+<marquee><h2>My profile</h2></marquee>
+<a href="https://www.w3schools.com">Visit W3Schools</a>
+
+<form>
+First Name: <input type="text" name="fname"> <br>
+Last Name: <input type="text"><br>
+Choose one option <br>
+Male <input type="radio" name="gender">
+Female <input type="radio" name="gender">
+<br>
+Music<input type="radio" name="hobby">
+Dance <input type="radio" name="hobby">
+<br>
+
+
+<input type="date">
+
+<input type="submit" value="submit">
+<input type="reset">
+
+</form>
+</body>
+</html>
